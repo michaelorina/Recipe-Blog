93122ac8e25ef1cb511ef54b3ee3c53947e46256
michaelorina <orinamichael4@gmail.com>
2023-04-04 22:05:07 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const { urlencoded } = require('express');
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(expressLayout);
+
+app.set('layout', './layouts/main');
+
+const routes = require('./server/routes/recipeRoutes.js');
+app.use('/', routes);
+
+app.listen(port, ()=> console.log(`Listening to Port ${port}`));
