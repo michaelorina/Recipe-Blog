5d902ac70dc83f779bfe28b86fa82f9800bb7a48
michaelorina <orinamichael4@gmail.com>
2023-04-11 10:51:49 +0300

    Hello World!

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.static('public'));
 app.use(expressLayout);
 
 app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
 const routes = require('./server/routes/recipeRoutes.js');
 app.use('/', routes);
